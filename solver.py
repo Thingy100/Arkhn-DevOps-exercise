010206c2b1d3202a2d4defb423cc8287af913ff8
Thingy100 <jocelynmoisan@yahoo.fr>
2021-01-24 15:32:23 +0100

    solver v1

---
+++ solver.py
@@ -0,0 +1,35 @@
+import random
+import requests
+from flask import Flask, request
+
+server = Flask(__name__)
+
+def verify_text(input_string):
+    list_open = []
+    for c in input_string:
+        if c in ["(", "[", "{"]:
+            list_open.append(c)
+        elif c in [")", "]", "}"]:
+            if not list_open:
+                return (f"INPUT: {input_string} -- RESULT: BAD INPUT : {c}\n")
+            x = list_open.pop()
+            if x == "(" and c !=")":
+                return(f"INPUT: {input_string} -- RESULT: BAD INPUT : {c}\n")
+            if x == "[" and c !="]":
+                return(f"INPUT: {input_string} -- RESULT: BAD INPUT : {c}\n")
+            if x == "{" and c !="}":
+                return(f"INPUT: {input_string} -- RESULT: BAD INPUT : {c}\n")
+    if not list_open:
+        return(f"INPUT: {input_string} -- RESULT: OK!\n")
+    return(f"INPUT: {input_string} -- RESULT: BAD INPUT : Need a close\n")
+
+@server.route("/solve", methods = ['POST', 'GET'])
+def solve_input():
+    input_string = request.get_data().decode()
+    if input_string == "":
+        input_string = requests.get('http://localhost:5000/input').content.decode()
+    print(verify_text(input_string))
+    return input_string + "\n"
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port = 5001)
